0620fde9d8e815d7c7fa855a1f88f7790c5df33a
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-04-17 22:54:31 +0300

    Add some functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101'
-        }}
-      >
-        React homework template
-      </div>
-    );
-  };
+import React, {Component} from "react";
+import { nanoid } from "nanoid";
+
+export class App  extends Component {
+    state = {
+        contacts: [
+          {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+          {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+          {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+          {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
+        filter: '',
+    }
+    getFilterValue = (event) => {
+        this.setState({filter: event.target.value.toLowerCase()})
+      }
+    
+    deleteContact = (event) => {
+        this.setState(prevState => ({
+          contacts: prevState.contacts.filter(value => value.id !== event.target.dataset.id)
+        }))
+        
+      }
+
+    addContact = ({name, number}) =>  {
+        if (this.state.contacts.some(value => value.name.toLowerCase() === name.toLowerCase())) {
+          alert(`${name} is already in contacts.`);
+          return;
+        }
+        if (this.state.contacts.some(value => value.number === number)) {
+          const findContact = this.state.contacts.find(option => option.number === number);
+          alert(`${number} is already in contacts as ${findContact.name}.`);
+          return;
+        }
+        this.setState(prevState =>({
+          contacts: [...prevState.contacts, {
+            id: nanoid(),
+            name,
+            number
+        }]
+          }));
+      }
+    
+    getVisibleContacts = (contacts, filter) => {
+        return contacts.filter(value =>  value.name.toLocaleLowerCase().includes(filter));
+      }
+
+    render() {
+        return (
+            <div>
+            <h1>Phonebook</h1>
+            
+            </div>
+        );
+      }
+}
