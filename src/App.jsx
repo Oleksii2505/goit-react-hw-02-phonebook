ca2853d944f193c876aeaa2fe8da7753f87fe86a
Oleksii Puzanov <alexey.puzanov@i.ua>
2023-04-18 18:53:27 +0300

    Done second part

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,81 @@
 import React, {Component} from "react";
 import { nanoid } from "nanoid";
+import { ContactForm } from "components/ContactForm";
+import { Filter } from "components/Filter";
+import { ContactList } from "components/ContactList";
+import { Container, TitlePhonebook, TitleContacts } from './App.styled';
+
+
+const initialContacts = [
+    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export class App  extends Component {
     state = {
-        contacts: [
-          {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-          {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-          {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-          {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-        ],
+        contacts: initialContacts,
         filter: '',
-    }
-    getFilterValue = (event) => {
-        this.setState({filter: event.target.value.toLowerCase()})
-      }
+    };
     
-    deleteContact = (event) => {
+    onFormSubmit = data => {
+        if (
+            this.state.contacts.some(
+                value => value.name.toLowerCase() === data.name.toLowerCase()
+            )
+        ) {
+            alert(`${data.name} is already in contacts.`);
+            return;
+        }
+        if (this.state.contacts.some(value => value.number === data.number)) {
+            const findContact = this.state.contacts.find(
+                option => option.number === data.number
+            );
+            alert(
+                `${data.number} is already in contacts as ${findContact.name}.`
+            );
+            return;
+        }
         this.setState(prevState => ({
-          contacts: prevState.contacts.filter(value => value.id !== event.target.dataset.id)
-        }))
-        
-      }
-
-    addContact = ({name, number}) =>  {
-        if (this.state.contacts.some(value => value.name.toLowerCase() === name.toLowerCase())) {
-          alert(`${name} is already in contacts.`);
-          return;
-        }
-        if (this.state.contacts.some(value => value.number === number)) {
-          const findContact = this.state.contacts.find(option => option.number === number);
-          alert(`${number} is already in contacts as ${findContact.name}.`);
-          return;
-        }
-        this.setState(prevState =>({
-          contacts: [...prevState.contacts, {
-            id: nanoid(),
-            name,
-            number
-        }]
-          }));
-      }
+            contacts: [...prevState.contacts, { ...data, id: nanoid() }],
+        }));
+    };
     
-    getVisibleContacts = (contacts, filter) => {
-        return contacts.filter(value =>  value.name.toLocaleLowerCase().includes(filter));
-      }
+    getFilteredContacts = () => {
+        const { contacts, filter } = this.state;
+        const normalizedFilter = filter.toLowerCase();
+        return contacts.filter(contact =>
+            contact.name.toLowerCase().includes(normalizedFilter)
+        );
+    };
+
+    changeFilter = e => {
+        this.setState({ filter: e.target.value });
+    };
+    
+    deleteContacts = contactId => {
+        this.setState(prevState => ({
+            contacts: prevState.contacts.filter(
+                contact => contact.id !== contactId
+            ),
+        }));
+    };
 
     render() {
+      const { filter } = this.state;
         return (
-            <div>
-            <h1>Phonebook</h1>
-            
-            </div>
+          <Container>
+          <TitlePhonebook>Phonebook</TitlePhonebook>
+          <ContactForm onSubmit={this.onFormSubmit} />
+
+          <TitleContacts>Contacts</TitleContacts>
+          <Filter value={filter} onFilter={this.changeFilter} />
+          <ContactList
+              list={this.getFilteredContacts()}
+              delContact={this.deleteContacts}
+          />
+      </Container>
         );
       }
 }
